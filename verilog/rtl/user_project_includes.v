b80b343f27c7b07024cd9200dcc431b30610c2fd
matt venn <matt@mattvenn.net>
2022-08-28 23:19:58 +0200

    add etpu

--- verilog/rtl/user_project_includes.v
+++ verilog/rtl/user_project_includes.v
@@ -3,9 +3,11 @@
 // +------------+--------------------+-----------------+--------------------------------------------------------+------------------------------------------+
 // | 0          | Function generator | Matt Venn       | https://github.com/mattvenn/wrapped_function_generator | 701095fd880ad3bb80d6cec1d214a04e5676a65d |
 // | 1          | ibnalhaytham       | Farhad Modaresi | https://github.com/sfmth/wrapped_ibnalhaytham          | 0627452464db56b813a3aae8899e8339a358fac9 |
+// | 3          | Educational tpu    | Camilo Soto     | https://github.com/tucanae47/wrapped_etpu              | 748d3cc0bcba8abb06acd40ab13ed89ff307ede6 |
 // +------------+--------------------+-----------------+--------------------------------------------------------+------------------------------------------+
 `include "wrapped_function_generator/wrapper.v" // 0
 `include "wrapped_ibnalhaytham/wrapper.v" // 1
+`include "wrapped_etpu/wrapper.v" // 3
 // shared projects
 `include "wb_bridge/src/wb_bridge_2way.v"
 `include "wb_openram_wrapper/src/register_rw.v"
